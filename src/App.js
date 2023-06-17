4cba7ce1c1ff4d5011f33e2addd0d72ab463d34b
MCF512 <egdem4enko@yandex.ru>
2023-06-17 18:33:18 +0300

    task 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 function App() {
+  const [todosCompleted, setTodosCompleted] = useState([])
+  const [todosNotCompleted, setTodosNotCompleted] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => {
+        setTodosCompleted(json.filter(item => {
+          return item.completed
+        }))
+        setTodosNotCompleted(json.filter(item => {
+          return !item.completed
+        }))
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={styles.App}>
+      <h2 className={styles.mainTitle}>Todo list</h2>
+
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <h3 className={styles.title}>Не выполнено</h3>
+          <div className={styles.todos}>
+            {todosCompleted.map(({ title }) => {
+              return (
+                <div className={styles.notCompletedTodo}>
+                  <p className={styles.titleNotCompleted}>{title}</p>
+                  <p className={styles.completed}>Не выполнено</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+
+        <div className={styles.wrapper}>
+          <h3 className={styles.title}>Выполнено</h3>
+          <div className={styles.todos}>
+            {todosCompleted.map(({ title }) => {
+              return (
+                <div className={styles.completedTodo}>
+                  <p className={styles.titleCompleted}>{title}</p>
+                  <p className={styles.completed}>Выполнено</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+
+      </div>
+    </div >
   );
 }
 
